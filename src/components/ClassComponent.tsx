334c747254685f89c8e432dad99ede59823801e5
Ginea Merrill <gineakanoe@gmail.com>
2021-11-10 14:35:07 -0500

    set up file structure

---
+++ src/components/ClassComponent.tsx
@@ -0,0 +1,30 @@
+//! Instruction:
+// Using typescript, create a class component and a functional component. The class component should 
+// reach out to the Geolocation API to grab your coordinates. Then using those 
+// coordinates, reach out to the Open Weather API and retrieve the weather 
+// information for your area.  Once you have the weather, it should be sent to the functional component to be displayed. 
+
+//* Geolocation API
+    // API DOCUMENTATION  -https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
+    // KEY  - 
+    // ENDPOINT  - 
+
+//* Open Weather API
+    // API DOCUMENTATION -https://openweathermap.org/api
+    // KEY  - 8aba18f043d15359afd51db8ca1c0c89
+    // ENDPOINT - api.openweathermap.org
+
+import React from 'react';
+
+class ClassComponent extends React.Component {
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ClassComponent;
---
+++ src/components/FunctionalComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";  
+
+const FunctionalComponent = () => {
+
+
+    return(
+        <div>
+        
+        </div>
+    )
+}
+
+export default FunctionalComponent;
