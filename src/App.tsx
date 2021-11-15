270b510667b64539a142f6e0e05abf9d8fd22893
Ginea Merrill <gineakanoe@gmail.com>
2021-11-14 20:46:39 -0500

    got temp, lat/long, and time to display

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import ClassComponent from './components/ClassComponent';
 
 const App: React.FunctionComponent = () => {
 
   return (
     <div className="App">
       <div className="verticalCenter">
-
+        <ClassComponent />
       </div>
     </div>
   );
--- src/components/FunctionalComponent.tsx
+++ src/components/FunctionalComponent.tsx
@@ -1,22 +1,16 @@
 import React from "react";  
 
-const FunctionalComponent = (props) => {
+const FunctionalComponent = (props: any) => {
 
 
     return(
         <div>
-            {
-                Object.keys(props.main).length === 0 && props.weather.length === 0 ? (
-                    <div>Loading...</div>
-                ) : (
-                    <div>
-                        <h1>{props.city}</h1>
-                        <h3>{Math.round(props.main.temp)}°</h3>
-                        <img src={`http://openweathermap.org/img/w/${props.weather[0].icon}.png`} alt='weather icon' style={{height: '50px', width: '50px'}} />
-                        <p style={{fontSize: '12px', marginTop: '3em'}}>Last update at {props.time}</p>
-                    </div>
-                )
-                }
+            <div>
+                <h1>Current Weather</h1>
+                <h3>{Math.round(props.weather)}°</h3>
+                <img src={`http://openweathermap.org/img/w/${props.weather[0]}.png`} alt='weather icon' style={{height: '50px', width: '50px'}}/>
+                <p>{props.lat}, {props.long} <br />Last update at {props.time}</p>
+            </div>
         </div>
     )
 }
